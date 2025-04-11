79d502e51f53f1835d2f82affb9ebc55e4535990
dafandikri <dafandikri@gmail.com>
2025-04-11 19:49:19 +0700

    feat: update ReviewController to use RequestBody for review submission and return response entity

--- src/main/java/id/ac/ui/cs/advprog/eventsphere/review/controller/ReviewController.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/review/controller/ReviewController.java
@@ -1,10 +1,17 @@
 package id.ac.ui.cs.advprog.eventsphere.review.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import id.ac.ui.cs.advprog.eventsphere.review.service.ReviewService;
+import id.ac.ui.cs.advprog.eventsphere.review.dto.ReviewDto;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -16,7 +23,13 @@ public class ReviewController {
     }
 
     @PostMapping("/reviews")
-    public void createReview(@RequestParam int rating, @RequestParam String comment) {
-        reviewService.createReview(rating, comment);
+    public ResponseEntity<Map<String, Object>> createReview(@RequestBody ReviewDto reviewDto) {
+        reviewService.createReview(reviewDto.getRating(), reviewDto.getComment());
+        
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", true);
+        response.put("message", "Review submitted successfully");
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
