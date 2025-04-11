df9df477afaf783a3d4b8e8b0396b9283e762881
cjoyy <joycalvin1604@gmail.com>
2025-04-11 19:17:39 +0700

    [GREEN] Implement Event n User DTOs

---
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/dto/EventResponseDTO.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.advprog.eventsphere.dto;
+
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class EventResponseDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime eventDate;
+    private String location;
+    private BigDecimal price;
+    private UserSummaryDTO organizer;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean isActive;
+}
