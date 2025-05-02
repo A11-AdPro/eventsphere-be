54b5bd781e4453a96561f32dd21f75e7da734d80
patriciatyas <patriciatyas11@gmail.com>
2025-05-02 18:51:29 +0700

    [RED] implement new method in CustomTopUp model

--- src/main/java/id/ac/ui/cs/advprog/eventsphere/topup/model/CustomTopUp.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/topup/model/CustomTopUp.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.eventsphere.topup.model;
 
 public class CustomTopUp implements TopUp {
     private final int amount;
+    private final String type;
     private final int MIN = 10000;
     private final int MAX = 1000000;
 
@@ -10,10 +11,16 @@ public class CustomTopUp implements TopUp {
             throw new IllegalArgumentException("Custom top-up amount must be between 10,000 and 1,000,000");
         }
         this.amount = amount;
+        this.type = "CUSTOM";
     }
 
     @Override
     public int getAmount() {
         return amount;
+    }
+
+    @Override
+    public String getType() {
+        return type;
     }
 }
