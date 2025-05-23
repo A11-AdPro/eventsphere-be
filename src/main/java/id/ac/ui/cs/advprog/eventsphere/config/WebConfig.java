9e9e40d062a75da4f2170fea2dfb275f3e7ccde3
patriciatyas <patriciatyas11@gmail.com>
2025-05-23 16:08:20 +0700

    [REFACTOR] Cors configuration

--- src/main/java/id/ac/ui/cs/advprog/eventsphere/config/WebConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/config/WebConfig.java
@@ -1,26 +1,15 @@
 package id.ac.ui.cs.advprog.eventsphere.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") // Next.js default port
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-
+public class WebConfig {
+    
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();  // Menyediakan RestTemplate sebagai bean
+        return new RestTemplate();
     }
-
+    
 }
